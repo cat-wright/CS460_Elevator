5c674620e07515d4420fd8cbcba27653ed631933
Dominic <ddelvecchio@unm.edu>
2018-04-04 11:22:46 -0600

    Added Cabin skeleton

---
+++ src/Cabin.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Dominic on 4/4/2018.
+ */
+public class Cabin
+{
+  
+  Cabin()
+  {
+    
+  }
+  
+  void moveCabin(Integer floor)
+  {
+    
+  }
+  
+  Boolean isCabinMoving()
+  {
+    return false;
+  }
+}
