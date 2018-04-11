9a47471cd91f89c6fbbb6c44b95586c9f11f245d
Dominic <ddelvecchio@unm.edu>
2018-04-11 12:35:28 -0600

    switched variables I had the queue backwards

--- src/Request/StateController.java
+++ src/Request/StateController.java
@@ -16,53 +16,53 @@ public class StateController
       
       if(cabin.getDirection() == Directions.UP && r1.getDirection() == Directions.UP && r2.getDirection() == Directions.UP)
       {
-        if(r1.getDestination() < r2.getDestination()) {return 1; }
-        else if(r1.getDestination() > r2.getDestination()) {return -1;}
+        if(r1.getDestination() < r2.getDestination()) {return -1; }
+        else if(r1.getDestination() > r2.getDestination()) {return 1;}
         else return 0;
       }
   
       else if(cabin.getDirection() == Directions.UP && r1.getDirection() == Directions.UP && r2.getDirection() == Directions.DOWN)
       {
-        if(cabin.getCabinLocation() < r1.getDestination() || cabin.getCabinLocation() == r1.getDestination()) {return 1;}
-        else if(cabin.getCabinLocation() > r1.getDestination()) {return -1;}
+        if(cabin.getCabinLocation() < r1.getDestination() || cabin.getCabinLocation() == r1.getDestination()) {return -1;}
+        else if(cabin.getCabinLocation() > r1.getDestination()) {return 1;}
       }
 
       else if(cabin.getDirection() == Directions.UP && r1.getDirection() == Directions.DOWN && r2.getDirection() == Directions.UP)
       {
-        if(cabin.getCabinLocation() < r2.getDestination()) {return -1;}
-        else if(cabin.getCabinLocation() > r2.getDestination()) {return 1;}
+        if(cabin.getCabinLocation() < r2.getDestination()) {return 1;}
+        else if(cabin.getCabinLocation() > r2.getDestination()) {return -1;}
       }
 
       else if(cabin.getDirection() == Directions.UP && r1.getDirection() == Directions.DOWN && r2.getDirection() == Directions.DOWN)
       {
-        if(r1.getDestination() > r2.getDestination()) {return 1;}
-        else if(r1.getDestination() < r2.getDestination()) {return -1;}
+        if(r1.getDestination() > r2.getDestination()) {return -1;}
+        else if(r1.getDestination() < r2.getDestination()) {return 1;}
         else return 0;
       }
   
       else if(cabin.getDirection() == Directions.DOWN && r1.getDirection() == Directions.DOWN && r2.getDirection() == Directions.DOWN)
       {
-        if(r1.getDestination() > r2.getDestination()) {return 1; }
-        else if(r1.getDestination() < r2.getDestination()) {return -1;}
+        if(r1.getDestination() > r2.getDestination()) {return -1; }
+        else if(r1.getDestination() < r2.getDestination()) {return 1;}
         else return 0;
       }
   
       else if(cabin.getDirection() == Directions.DOWN && r1.getDirection() == Directions.DOWN && r2.getDirection() == Directions.UP)
       {
-        if(cabin.getCabinLocation() > r1.getDestination() || cabin.getCabinLocation() == r1.getDestination()) {return 1;}
-        else if(cabin.getCabinLocation() < r1.getDestination()) {return -1;}
+        if(cabin.getCabinLocation() > r1.getDestination() || cabin.getCabinLocation() == r1.getDestination()) {return -1;}
+        else if(cabin.getCabinLocation() < r1.getDestination()) {return 1;}
       }
   
       else if(cabin.getDirection() == Directions.DOWN && r1.getDirection() == Directions.UP && r2.getDirection() == Directions.DOWN)
       {
-        if(cabin.getCabinLocation() > r2.getDestination()) {return -1;}
-        else if(cabin.getCabinLocation() < r2.getDestination()) {return 1;}
+        if(cabin.getCabinLocation() > r2.getDestination()) {return 1;}
+        else if(cabin.getCabinLocation() < r2.getDestination()) {return -1;}
       }
   
       else if(cabin.getDirection() == Directions.DOWN && r1.getDirection() == Directions.UP && r2.getDirection() == Directions.UP)
       {
-        if(r1.getDestination() < r2.getDestination()) {return 1;}
-        else if(r1.getDestination() > r2.getDestination()) {return -1;}
+        if(r1.getDestination() < r2.getDestination()) {return -1;}
+        else if(r1.getDestination() > r2.getDestination()) {return 1;}
         else return 0;
       }
       return 0;
@@ -79,6 +79,10 @@ public class StateController
     requestQueue.add(r1);
   }
   
+  public Request getRequest()
+  {
+    return requestQueue.remove();
+  }
   
   
   
