8c8ed0ca486c00711e6e9a63df9d4bbdcbc69aa8
Vincent Crespin <crespinv@unm.edu>
2018-04-20 15:44:10 -0600

    updated doors stuff

--- src/Doors/DoorController.java
+++ src/Doors/DoorController.java
@@ -1,10 +1,33 @@
 package Doors;
 
 public class DoorController {
-    private DoorMotor doorMotor;
+    private DoorMotor floorDoorMotor;
+    private DoorMotor cabinDoorMotor;
 
-    public void operateDoor(DoorType type){
-        doorMotor = new DoorMotor(type);
-        doorMotor.run();
+    public void operateDoor(){
+        floorDoorMotor = new DoorMotor(DoorType.FLOOR);
+        cabinDoorMotor = new DoorMotor(DoorType.CABIN);
+        cabinDoorMotor.run();
+        floorDoorMotor.run();
     }
+
+    public DoorState getFloorState(){
+        return floorDoorMotor.getDoorState();
+    }
+    public DoorState getCabinState(){
+        return floorDoorMotor.getDoorState();
+    }
+    public void openFloorDoor(boolean canDoorOpen){
+
+    }
+    public void closeFloorDoor(boolean canDooClose){
+
+    }
+    public void openCabinDoor(boolean canDoorOpen){
+
+    }
+    public void closeCabinDoor(boolean canDooClose){
+
+    }
+
 }
--- src/Doors/DoorMotor.java
+++ src/Doors/DoorMotor.java
@@ -2,18 +2,22 @@ package Doors;
 
 public class DoorMotor extends Thread {
     private Door door;
+    private boolean canDoorOpen;
+    private boolean canDoorClose;
 
     public DoorMotor (DoorType type){
         this.door = new Door(type);
+        this.canDoorOpen = false;
+        this.canDoorClose = false;
     }
 
     @Override
     public void run() {
-        if(door.shouldDoorClosed(door)){
+        if(door.shouldDoorClosed(door) && canDoorClose){
             door.closeDoors(door);
             sleep(2000);
         }
-        else if(door.shouldDoorOpen(door)){
+        else if(door.shouldDoorOpen(door) && canDoorOpen){
             door.shouldDoorOpen(door);
             sleep(2000);
         }
@@ -26,5 +30,17 @@ public class DoorMotor extends Thread {
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public DoorState getDoorState(){
+        return door.getDoorState();
+    }
+
+    public void setDoorOpen(boolean canDoorOpen){
+        this.canDoorOpen = canDoorOpen;
+    }
+
+    public void setDoorClose(boolean canDoorClose){
+        this.canDoorClose = canDoorClose;
     }
 }
