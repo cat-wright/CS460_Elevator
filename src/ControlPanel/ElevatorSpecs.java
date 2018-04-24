d7cf641dba99e873126bf2eedd0fbe7095f57e2d
Catherine Wright <wrightc@unm.edu>
2018-04-23 22:17:36 -0600

    GUI accepts list and gives maintenance/lock status

--- src/ControlPanel/ElevatorSpecs.java
+++ src/ControlPanel/ElevatorSpecs.java
@@ -5,24 +5,20 @@ import Request.*;
 public class ElevatorSpecs {
 
     private boolean isMoving;
-    private Request currentRequest;
+    //private Request currentRequest;
     private Integer currentFloor;
     private Directions direction;
 
-    public ElevatorSpecs(boolean isMoving, Request currentRequest, Integer currentFloor, Directions direction)
+    public ElevatorSpecs(boolean isMoving, Integer currentFloor, Directions direction)
     {
         this.isMoving = isMoving;
-        this.currentRequest = currentRequest;
+        //this.currentRequest = currentRequest;
         this.currentFloor = currentFloor;
         this.direction = direction;
     }
 
     public boolean isMoving() {
         return isMoving;
-    }
-
-    public Request getCurrentRequest() {
-        return currentRequest;
     }
 
     public Integer getCurrentFloor() {
